64a8a1d48c0e639d384ce671bda040ebbd272d0c
Shivam Chaturvedi <devs4v@gmail.com>
2013-11-11 12:07:44 +0530

    changes to rightinfobox
    reduced the padding of the rightinfobox and changed table's background
    color
    moved the css for the table produced to the css file

--- backend/getdetails.js
+++ backend/getdetails.js
@@ -25,7 +25,7 @@ function getDetails(file){
 }
 
 function mediaDetailBook(root){	
-	var detail = "<table style='background-color: #6699AA;box-shadow: 0 0 1px 1px #6699AA;width: 98%;margin-left: 2px;color: white;'><tr><td colspan='2' style='background-color: rgba(255, 255, 255, 0.5);'>Visual File Information:</td></tr>";
+	var detail = "<table><tr><td colspan='2' style='background-color: rgba(255, 255, 255, 0.5);'>Visual File Information:</td></tr>";
 	detail += "<tr><td style='background-color: rgba(238, 238, 238, 0.5);text-align: right;'>FileFormat:</td><td>" + root.find("FileFormat Name").text().trim() + "</td></tr>";
 	detail += "<tr><td style='background-color: rgba(238, 238, 238, 0.5);text-align: right;'>FileSize:</td><td>" + root.find("FileSize").text().trim() + "Bytes</td></tr>";
 	detail += "<tr><td colspan='2' style='background-color: rgba(255, 255, 255, 0.5);'>Visual File Coding:</td></tr>";
@@ -36,7 +36,7 @@ function mediaDetailBook(root){
 }
 
 function mediaDetailMusic(root){
-	var detail = "<table style='background-color: #6699AA;box-shadow: 0 0 1px 1px #6699AA;width: 98%;margin-left: 2px;color: white;'><tr><td colspan='2' style='background-color: rgba(255, 255, 255, 0.5);'>Audio File Information:</td></tr>";
+	var detail = "<table><tr><td colspan='2' style='background-color: rgba(255, 255, 255, 0.5);'>Audio File Information:</td></tr>";
 	detail += "<tr><td style='background-color: rgba(238, 238, 238, 0.5);text-align: right;'>Medium:</td><td>" + root.find("Medium Name").text().trim() + "</td></tr>";
 	detail += "<tr><td style='background-color: rgba(238, 238, 238, 0.5);text-align: right;'>FileFormat:</td><td>" + root.find("FileFormat Name").text().trim() + "</td></tr>";
 	detail += "<tr><td style='background-color: rgba(238, 238, 238, 0.5);text-align: right;'>BitRate:</td><td>" + root.find("BitRate").text().trim() + "</td></tr>";
